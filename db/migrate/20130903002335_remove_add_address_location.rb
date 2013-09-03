d2339e67a254e46fa6cf29b47214b84088cefc06
Simon <simon.taranto@gmail.com>
2013-09-02 18:29:30 -0600

    remove drop column in migration

--- db/migrate/20130903002335_remove_add_address_location.rb
+++ db/migrate/20130903002335_remove_add_address_location.rb
@@ -1,6 +1,5 @@
 class RemoveAddAddressLocation < ActiveRecord::Migration
   def self.up
-  	remove_column :attendees, :location_id
     remove_column :attendees, :address
     add_column :attendees, :location_id, :integer
   end
