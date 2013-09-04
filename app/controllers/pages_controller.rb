4e793d9ea69b893195c94953c9ed3085028b1348
Simon <simon.taranto@gmail.com>
2013-09-03 21:49:01 -0600

    get maps working locally

--- app/controllers/pages_controller.rb
+++ app/controllers/pages_controller.rb
@@ -1,5 +1,7 @@
 class PagesController < ApplicationController
   def home
   	@attendee = Attendee.new
+    locations = Location.all
+    @locations_json = locations.to_gmaps4rails
   end
 end
--- app/models/location.rb
+++ app/models/location.rb
@@ -2,4 +2,13 @@ class Location < ActiveRecord::Base
   validates_presence_of :city_name
   
   has_many :attendees
+
+  geocoded_by :city_name
+  after_validation :geocode
+
+  acts_as_gmappable
+
+  def gmaps4rails_address
+    "#{self.city_name}"
+  end
 end
