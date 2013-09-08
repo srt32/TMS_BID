3c90b983523869478cb0967148b0c80a138e5e2e
Simon Taranto <simon.taranto@gmail.com>
2013-09-08 14:00:51 -0600

    add seed data for 2 locations and one attendee

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,7 @@
 #
 #   cities = City.create([{ name: 'Chicago' }, { name: 'Copenhagen' }])
 #   Mayor.create(name: 'Emanuel', city: cities.first)
+
+locations = Location.create([{ city_name: 'New York City, NY' }, { city_name: 'San Francisco, CA' }])
+
+Attendee.create( name: 'Nigel', email: 'Nigel@example.com', attendees: 1, location_id: locations.first, zip: '12345')
