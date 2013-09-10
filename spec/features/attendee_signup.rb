0cac34bb58c994c9c076f142bf61cecced9ae7d8
Simon Taranto <simon.taranto@gmail.com>
2013-09-09 18:47:39 -0600

    add first feature test for user sign up

--- spec/features/attendee_signup.rb
+++ spec/features/attendee_signup.rb
@@ -1,20 +1,20 @@
 require 'spec_helper'
 
 describe "the attendee signup process", :type => :feature do
-  # before :each do
-  #   User.make(:email => 'user@example.com', :password => 'caplin')
-  # end
+  before :each do
+    Location.create(:city_name => "New York City, NY")
+  end
 
   it "signs me up" do
     visit root_path
     within("#sign-up-form") do
-      fill_in 'Name', :with => ''
-      fill_in 'Email', :with => ''
-      fill_in 'Nearest city', :with => '' # CHOOSE CITY
-      fill_in 'Zip code', :with => ''
-      fill_in 'Attendees', :with => '' # CHOOSE 1
+      fill_in 'Name', :with => 'Nigel'
+      fill_in 'Email', :with => 'Llama@example.com'
+      select("New York City, NY", :from => "attendee[location_id]")
+      fill_in 'Zip', :with => '01234'
+      select("1", :from => "attendee[attendees]")
     end
-    click_link 'Submit'
+    click_button 'Submit'
     expect(page).to have_content 'Great, we have your info'
   end
 end
